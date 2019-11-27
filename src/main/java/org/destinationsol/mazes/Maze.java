c403de539c2db1a393e79c1a7ce230997c40fb07
NicholasBatesNZ <NicholasBatesNZ@users.noreply.github.com>
2019-11-27 22:01:16 +1300

    It runs! But doesn't quite work...

---
+++ src/main/java/org/destinationsol/mazes/Maze.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2019 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.destinationsol.mazes;
+
+import com.badlogic.gdx.math.Vector2;
+import org.destinationsol.Const;
+import org.destinationsol.common.SolRandom;
+import org.destinationsol.game.BuildableSystem;
+import org.destinationsol.game.ConfigurationSystem;
+import org.destinationsol.game.ShipConfig;
+import org.destinationsol.game.SolCam;
+import org.destinationsol.game.SolGame;
+import org.destinationsol.game.UpdateAwareSystem;
+import org.destinationsol.game.chunk.SpaceEnvConfig;
+
+import java.util.List;
+
+public class Maze implements UpdateAwareSystem, BuildableSystem {
+    private MazeConfigs configs;
+    private MazeConfig config;
+    private Vector2 position;
+    private float radius;
+    private float damagePerSecond;
+    private boolean areObjectsCreated;
+
+    @Override
+    public void build(List<ConfigurationSystem> configurationSystems, Vector2 position, float radius) {
+        for (ConfigurationSystem system : configurationSystems) {
+            //try {
+                configs = (MazeConfigs) system;
+            //} catch (Exception ignore) { }
+        }
+        this.config = SolRandom.seededRandomElement(configs.getConfigurations());
+        this.position = position;
+        this.radius = radius;
+        damagePerSecond = getMazeDps(this.config);
+    }
+
+    @Override
+    public void update(SolGame game, float timeStep) {
+        SolCam cam = game.getCam();
+        Vector2 camPos = cam.getPosition();
+        if (!areObjectsCreated && camPos.dst(position) < radius + Const.CAM_VIEW_DIST_JOURNEY * 2) {
+            new MazeBuilder().build(game, this);
+            areObjectsCreated = true;
+        }
+    }
+
+    private static float getMazeDps(MazeConfig c) {
+        float outer = getShipConfListDps(c.outerEnemies);
+        float inner = getShipConfListDps(c.innerEnemies);
+        float res = inner < outer ? outer : inner;
+        return res * 1.25f;
+    }
+
+    private static float getShipConfListDps(List<ShipConfig> ships) {
+        float maxDps = 0;
+        for (ShipConfig e : ships) {
+            if (maxDps < e.dps) {
+                maxDps = e.dps;
+            }
+        }
+        return maxDps;
+    }
+
+    MazeConfig getConfig() {
+        return config;
+    }
+
+    @Override
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    /**
+     * @return the full radius including the exterior border.
+     */
+    @Override
+    public float getRadius() {
+        return radius;
+    }
+
+    @Override
+    public float getMaximumRadius() {
+        return 40f;
+    }
+
+    @Override
+    public float getDamagePerSecond() {
+        return damagePerSecond;
+    }
+
+    @Override
+    public float getBorder() {
+        return MazeBuilder.BORDER;
+    }
+
+    @Override
+    public String getTexture() {
+        return "engine:mapObjects/maze";
+    }
+
+    @Override
+    public SpaceEnvConfig getSpaceEnvironmentConfiguration() {
+        return config.envConfig;
+    }
+}
---
+++ src/main/java/org/destinationsol/mazes/MazeConfigs.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2019 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.destinationsol.mazes;
+
+import org.destinationsol.assets.json.Validator;
+import org.destinationsol.game.ConfigurationSystem;
+import org.json.JSONObject;
+import org.destinationsol.assets.Assets;
+import org.destinationsol.assets.json.Json;
+import org.destinationsol.files.HullConfigManager;
+import org.destinationsol.game.item.ItemManager;
+import org.terasology.assets.ResourceUrn;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class MazeConfigs implements ConfigurationSystem {
+    private List<MazeConfig> configs;
+
+    @Override
+    public void generateConfigurations(HullConfigManager hullConfigs, ItemManager itemManager) {
+        configs = new ArrayList<>();
+        final Set<ResourceUrn> configUrns = Assets.getAssetHelper().list(Json.class, "[a-zA-Z0-9]*:mazesConfig");
+        for (ResourceUrn configUrn : configUrns) {
+            JSONObject rootNode = Validator.getValidatedJSON(configUrn.toString(), "engine:schemaMazesConfig");
+            for (String name : rootNode.keySet()) {
+                if (!(rootNode.get(name) instanceof JSONObject))
+                    continue;
+                JSONObject mazeNode = rootNode.getJSONObject(name);
+                String moduleName = configUrn.getModuleName().toString();
+                MazeConfig c = MazeConfig.load(moduleName, name, mazeNode, hullConfigs, itemManager);
+                configs.add(c);
+            }
+
+        }
+    }
+
+    public List<MazeConfig> getConfigurations() {
+        return configs;
+    }
+}
